2d23a0eb011649b5520f2956f1a1f8215188dfa9
Daedluz <hiiammichael0520@gmail.com>
2021-10-10 13:37:13 +0800

    Changed layout

--- hw2/main.js
+++ hw2/main.js
@@ -8,11 +8,11 @@ function to_main (e)
 {
     
     var mainImg = document.getElementsByClassName("Main_pic");
-    var main_src = mainImg[0].children[0].src;
+    var main_src = mainImg[0].children[0].children[0].src;
     var prev_source = e.children[0].src;
 
     console.log(main_src)
 
-    mainImg[0].children[0].src = prev_source;
+    mainImg[0].children[0].children[0].src = prev_source;
     e.children[0].src = main_src;
 }
