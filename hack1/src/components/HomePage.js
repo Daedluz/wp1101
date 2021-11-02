63d84f3d1b03fe57357102c6a53cba35868ad4d5
Daedluz <hiiammichael0520@gmail.com>
2021-11-02 12:28:27 +0800

    Still working on Todo 6

--- hack1/src/components/HomePage.js
+++ hack1/src/components/HomePage.js
@@ -22,13 +22,34 @@ const HomePage = ({startGameOnClick, mineNumOnChange, boardSizeOnChange, mineNum
           <p className = 'title'>MineSweeper</p>
             {/* -- TODO 1-1 -- */}
             <button className="btn" onClick={startGameOnClick}>Start Game</button>
-            {/* <br/> */}
           
             {/* -- TODO 6-2 -- */}
             {/* Useful Hint: <input type = 'range' min = '...' max = '...' defaultValue = '...'> */}
             {/* Useful Hint: Error color: '#880000', default text color: '#0f0f4b', invisible color: 'transparent' */}
             {/* Reminder: The defaultValue of 'mineNum' is 10, and the defaultValue of 'boardSize' is 8. */}
-            {/* <button className="btn" onClick={() => setShowPanel(!showPanel)}>Difficulty Adjustment</button> */}
+            <div className="controlContainer">
+              <button className="btn" onClick={() => setShowPanel(!showPanel)}>Difficulty Adjustment</button>
+              {
+                showPanel ? 
+                <div className="controlWrapper">
+                  <div className="error"></div>
+                  <div className="controlPanel">
+                    <div className="controlCol">
+                      <p className="controlTitle">Mines Number</p>
+                      <input type="range" step='1' min="1" max="50" defaultValue="10"/>
+                      <p className="controlNum" style={error ? {color: "#880000"} : {color:  "#0f0f4b"}}></p>
+                    </div>
+
+                    <div className="controlCol">
+                      <p className="controlTitle">Board Size (n×n)</p>
+                      <input type="range" step='1' min="1" max="20" defaultValue="8"/>
+                      <p className="controlNum" style={error ? {color: "#880000"} : {color:  "#0f0f4b"}}></p>
+                    </div>
+                  </div>
+                </div> : ""
+              }
+            </div>
+            
         </div>
     );
 
